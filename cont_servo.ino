1ae1991b44d257f1a357a4198c57fb80da398458
Glenn Salaman <g.salaman@comcast.net>
2025-05-20 07:55:31 -0600

    Update cont_servo.ino

--- cont_servo.ino
+++ cont_servo.ino
@@ -1,24 +1,74 @@
 /* PWM Motor Control Sketch */
 
 
-/* Uno Version
+/* Uno Version only...uses serial*/
 #define PWM_PIN 3
 #define DIAL_CTL_PIN A0
-#define DEBUG_SERIAL
-#endif
-*/
 
-/* Tiny Version */
-#define PWM_PIN 0
-#define DIAL_CTL_PIN A1
 
-int hi_time_us;
+int hi_time_us=1000;
+
+void set_us_delay(int number)
+{
+  hi_time_us = number;
+  Serial.print("Pulse set to ");
+  Serial.println(hi_time_us);
+}
+
+void process_serial( void )
+{
+  char c;
+  static bool building_number=false;
+  static uint32_t  number = 0;
+  
+  /* commands:
+   *  f = fast motion
+   *  d = switch direction
+   *  x = stop
+   *  s = slow motion
+   *  any number sets the microsecond delay
+   */
+  if (Serial.available())
+  {
+    c = Serial.read();
+
+    if (c >= '0' && c <= '9')
+    {
+      building_number = true;
+      
+      /* continue building our number input */
+      number = number * 10;
+      number = number + (c - '0');   
+    }
+
+    else
+    {
+        
+      switch (c)
+      {
+        case '\n':
+        case '\r':
+        case '*':
+          if (building_number)
+          {
+            set_us_delay(number);
+        
+            number = 0;
+            building_number=false;
+          }
+        break;
+
+        default:
+          Serial.print("Unknown input: ");
+          Serial.println(c);
+      }
+    }
+  }
+}
 
 void setup()
 {
-  #ifdef DEBUG_SERIAL
   Serial.begin(9600);
-  #endif
 
   pinMode(PWM_PIN, OUTPUT);
 
@@ -27,24 +77,13 @@ void setup()
 
 void loop()
 {
-  int dial_val;
-  
-  // Hacky, bit banged busy wait version
-  dial_val = analogRead(DIAL_CTL_PIN);
-  hi_time_us = 1000+dial_val;
 
-  // if we're in the middle of the range, I'm just gonna write it low so that
-  // we avoid servo wobble
-  if ((dial_val < 600) && (dial_val > 400))
-  {
-    digitalWrite(PWM_PIN, LOW);
-  }
-  else
-  {
+    process_serial();
+    
     // do one pwm pulse
     digitalWrite(PWM_PIN, HIGH);
     delayMicroseconds(hi_time_us);
     digitalWrite(PWM_PIN, LOW);
     delayMicroseconds(20000-hi_time_us);
-  }
+
 }
